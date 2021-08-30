34165ffe2a28c348a2eee53a70f4b820b175c047
Jiyur <53509652+Jiyur@users.noreply.github.com>
2021-08-30 19:52:00 +0700

    them chuc nang

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ var getElement=document.querySelectorAll('div[column="6"]>[role="gridcell"]')
 console.log(getElement)
 var getText=getElement[3].textContent
 console.log(getText)
+console.log(getText)
